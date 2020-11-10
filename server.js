ee3c5876c84fb43825b5e3506ef033dda4187d4e
Consolekakao <console@kakao.com>
2020-11-11 02:37:30 +0900

    Update README.md

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const axios = require("axios");
 var http = require("http").createServer(app);
 
 const cors = require("cors");
